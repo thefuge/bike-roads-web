4455593eb96949e91f0f5427a0bf9254c9ee1852
system <toropchin20@gmail.com>
2024-12-06 12:14:29 +0300

    KAKMHEPLOXO

--- client/src/components/ui/ReviewCard.jsx
+++ client/src/components/ui/ReviewCard.jsx
@@ -1,13 +1,20 @@
-
 import Card from "react-bootstrap/Card";
-
+import { format } from 'date-fns';
+import { ru } from 'date-fns/locale';
 
 export default function ReviewCard({ review }) {
+  const formattedDate = format(
+    new Date(review.createdAt),
+    "d MMMM yyyy, HH:mm:ss",
+    { locale: ru }
+  );
   return (
     <Card style={{ width: "18rem" }}>
       <Card.Body>
         <Card.Title>Оценка: {review.routeRate}</Card.Title>
         <Card.Text>Отзыв: {review.routeReview}</Card.Text>
+        <Card.Text>Автор: {review.userName}</Card.Text>
+        <Card.Text>Дата: {formattedDate}</Card.Text>
       </Card.Body>
     </Card>
   );
